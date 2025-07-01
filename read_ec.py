3010c7a9b7cdcd9305baabaf238a7c956e345d9a
Niels <niels.novotny@gmail.com>
2025-07-01 15:54:46 -0600

    parsing etc. logging off...

--- read_ec.py
+++ read_ec.py
@@ -8,14 +8,12 @@ class CalibratedECReader:
         self.serial_port = '/dev/ttyUSB1'
         self.baud_rate = 9600
 
-    def send_command(ser, cmd):
+    def send_command(self,ser, cmd):
         full_cmd = cmd + '\r'
         ser.write(full_cmd.encode())
         time.sleep(0.5)
         response = ser.readline().decode().strip()
-        print(f">>> {cmd}")
-        print(f"<<< {response}")
-        return response
+        return response.split(",")[0]
     
     def read_raw_ec(self):
         try:
@@ -25,15 +23,9 @@ class CalibratedECReader:
         except Exception as e:
             print(f"Error reading EC: {e}")
             return None
-    
-    def read_ec(self):
-        raw_value = self.read_raw_ec()
-        raw_value.split(",")
-        return float(raw_value[:4])
-
 
 if __name__ == "__main__":
     reader = CalibratedECReader()
     while True:
-        print(reader.read_ec())
         time.sleep(.5)
+        print(reader.read_raw_ec())
