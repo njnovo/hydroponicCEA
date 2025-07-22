26fb2d444c7a79d2edc4db850e891c5c9d23b3a0
njnovo <75146085+njnovo@users.noreply.github.com>
2025-07-22 14:44:20 -0600

    if ts doesn't work I'm cooked

--- read_sensors/read_ph.py
+++ read_sensors/read_ph.py
@@ -5,16 +5,21 @@ class CalibratedPHReader:
     def __init__(self):
         self.serial_port = '/dev/ttyUSB1'
         self.baud_rate = 9600
+
+    def send_command(self,ser, cmd):
+        full_cmd = cmd + '\r'
+        ser.write(full_cmd.encode())
+        time.sleep(0.5)
+        response = ser.readline().decode().strip()
+        return response.split(",")[0]
     
     def read_ph(self):
         try:
             with serial.Serial(self.serial_port, self.baud_rate, timeout=1) as ser:
-                ser.write(b'r\r')
-                time.sleep(1.0)
-                response = ser.readline().decode().strip()
-                return float(response)
+                return float(self.send_command(ser, "R"))/1000
+                
         except Exception as e:
-            print(f"Error reading pH: {e}")
+            print(f"Error reading EC: {e}")
             return None
 
 if __name__ == "__main__":
